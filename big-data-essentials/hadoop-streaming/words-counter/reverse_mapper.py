6bf08668a4de975ac3b80b6cabb7638e9df257ad
Claudiu Soare <csoare7@gmail.com>
2017-12-05 23:28:03 +0000

    working version

--- big-data-essentials/hadoop-streaming/words-counter/reverse_mapper.py
+++ big-data-essentials/hadoop-streaming/words-counter/reverse_mapper.py
@@ -1,16 +1,10 @@
 import sys
 
 
-def main():
-    for line in sys.stdin:
-        try:
-            word, count = line.strip().split('\t', 1)
-            count = int(count)
-            print("{0}\t{1}".format(count, word))
-        except ValueError as e:
-            raise e
-            continue
-
-
-if __name__ == '__main__':
-    main()
+for line in sys.stdin:
+    try:
+        word, count = line.strip().split('\t', 1)
+        count = int(count)
+        print("{0}\t{1}".format(count, word))
+    except ValueError as e:
+        continue
